b9e718e0c65c4a86614244fb688331f662bec5d2
Dynatrace-Asad-Ali <asad.ali@dynatrace.com>
2025-04-16 15:20:49 -0700

    Debug commands added

--- .devcontainer/deployment.sh
+++ .devcontainer/deployment.sh
@@ -1,3 +1,4 @@
+echo "In deployment.sh" > /workspaces/$RepositoryName/status.out
 sed -i "s,GROQ_API_KEY_TOREPLACE,$GROQ_API_KEY," /workspaces/$RepositoryName/setEnv.sh
-sed -i "s,GROQ_API_KEY_TOREPLACE,$PHI_API_KEY," /workspaces/$RepositoryName/setEnv.sh
-sed -i "s,GROQ_API_KEY_TOREPLACE,$OPENAI_API_KEY," /workspaces/$RepositoryName/setEnv.sh
+sed -i "s,PHI_API_KEY_TOREPLACE,$PHI_API_KEY," /workspaces/$RepositoryName/setEnv.sh
+sed -i "s,OPENAI_API_KEY_TOREPLACE,$OPENAI_API_KEY," /workspaces/$RepositoryName/setEnv.sh
