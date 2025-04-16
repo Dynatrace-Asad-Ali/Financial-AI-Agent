1acd39897718b457619f9a73162cd6d0f50e8ea5
Dynatrace-Asad-Ali <asad.ali@dynatrace.com>
2025-04-16 15:11:31 -0700

    Fixed sed command

--- .devcontainer/deployment.sh
+++ .devcontainer/deployment.sh
@@ -1,1 +1,3 @@
-sed -i "s,GROQ_API_KEY_TOREPLACE,$DT_URL," /workspaces/$RepositoryName/dynatrace/dynakube.yaml
+sed -i "s,GROQ_API_KEY_TOREPLACE,$GROQ_API_KEY," /workspaces/$RepositoryName/setEnv.sh
+sed -i "s,GROQ_API_KEY_TOREPLACE,$PHI_API_KEY," /workspaces/$RepositoryName/setEnv.sh
+sed -i "s,GROQ_API_KEY_TOREPLACE,$OPENAI_API_KEY," /workspaces/$RepositoryName/setEnv.sh
