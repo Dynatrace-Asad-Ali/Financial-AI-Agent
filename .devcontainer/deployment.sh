dc499b88d5320b69ca33b4212e2b0ccdc609a982
Dynatrace-Asad-Ali <asad.ali@dynatrace.com>
2025-04-17 07:00:43 -0700

    Removed main py startup cmd

--- .devcontainer/deployment.sh
+++ .devcontainer/deployment.sh
@@ -3,6 +3,3 @@ sed -i "s,PHI_API_KEY_TOREPLACE,$PHI_API_KEY," /workspaces/$RepositoryName/setEn
 sed -i "s,OPENAI_API_KEY_TOREPLACE,$OPENAI_API_KEY," /workspaces/$RepositoryName/setEnv.sh
 
 source /workspaces/$RepositoryName/setEnv.sh
-
-python -m venv financial-agent && source financial-agent/bin/activate && pip install -r requirements.txt
-python main.py &
